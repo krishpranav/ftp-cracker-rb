aeca7a4a6379a1dd0c807cbd3ef9558801b025c1
krishpranav <krisna.pranav@gmail.com>
2021-02-22 17:32:04 +0530

    done changes

--- ftpcracker.rb
+++ ftpcracker.rb
@@ -1,2 +1,25 @@
 #!/usr/bin/env/ruby
 
+#imports
+
+require "socket"
+require "monitor"
+
+module Net
+
+
+	class FTPError < StandardError; end
+	class FTPReplyError < FTPError; end
+	class FTPTempError < FTPError; end
+	class FTPPermError < FTPError; end
+	class FTPProtoError < FTPError; end
+
+	class FTP
+		include MonitorMixin
+
+
+	FTP_PORT = 21
+	CRLF = "\r\n"
+	DEFAULT_BLOCKSIZE = 4096
+
+	attr_accessor :binary

1e47875509a2de539cb77c42ed36e9757fa08793
krishpranav <krisna.pranav@gmail.com>
2021-02-23 16:48:40 +0530

    done changes in ftpcracker.rb commit 1 on 23 Feb 2021

--- ftpcracker.rb
+++ ftpcracker.rb
@@ -15,11 +15,44 @@ module Net
 	class FTPProtoError < FTPError; end
 
 	class FTP
-		include MonitorMixin
+		include MonitorMixin 
 
 
 	FTP_PORT = 21
 	CRLF = "\r\n"
 	DEFAULT_BLOCKSIZE = 4096
 
-	attr_accessor :binary
+	attr_accessor :binary 
+
+	attr_accessor :passive
+
+	attr_accessor :debug_mode
+
+	attr_accessor :resume
+
+	attr_accessor :welcome
+
+	attr_reader :last_response_code
+
+	alias lastresp last_response_code
+
+	attr_reader :last_response
+
+	# If a block is given it is passed the +FTP+ object
+
+	def FTP.open(host, user = nil, passwd = nil, acct = nil)
+		if block_given?
+			ftp = new(host, user, passwd, acct)
+			begin 
+				yield ftp
+			ensure
+				ftp.close
+			end
+		else
+			new(host, user, passwd, acct)
+		end
+	end
+
+
+
+

2a120322874228fdc9cf29a031f2a504435d9352
krishpranav <krisna.pranav@gmail.com>
2021-02-23 16:53:54 +0530

    done changes in ftpcracker.rb commit 2 on 23 Feb 2021

--- ftpcracker.rb
+++ ftpcracker.rb
@@ -54,5 +54,53 @@ module Net
 	end
 
 
+	def initialize(host = nil, user = nil, passwd = nil, acct = nil)
+		super()
+		@binary = true 
+		@passive = false
+		@debug_mode = false
+		@resume = false
+		if host 
+			connect(host)
+			if user 
+				login(user, passwd, acct)
+			end
+		end
+	end
+
+	#obsolete
+
+	def return_code=(s)
+		$stderr.puts("warning: Net::FTP#return_code= is obsolete and do nothing")
+	end
+
+
+	def open_socket(host, port)
+		if defined? SOCKSsocket and ENV["SOCKS_SERVER"]
+			@passive = true
+			return SOCKSocket.open(host, port)
+		else
+			return TCPSocket.open(host, port)
+		end
+	end
+	private :open_socket
+
+
+	def connect(host, port = FTP_PORT)
+		if @debug_mode
+			print "connect: ", host, ", ", port, "\n"
+		end
+		synchronize do 
+			@sock = open_socket(host, port)
+			voidresp
+		end
+	end
+
+
+
+
+
+
+
 
 
